f0380f0d288bb56d05f9ed1b4de93af7b3d3ffa3
Hendryboyz <Hendry.js1247@gmail.com>
2022-06-10 11:51:06 +0800

    initial ailabs asr sdk project

---
+++ ailabs-asr/streaming.py
@@ -0,0 +1,11 @@
+def streaming(key: str):
+  """
+  ASR streaming to transcribe the voice to text
+  
+  Args:
+      key (str): the key applied in dev-console
+  Returns:
+      None
+  """
+  
+  print(f'Streaming with key: {key}')
